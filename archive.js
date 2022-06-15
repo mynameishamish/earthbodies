a38b3bd4384aa1883455cc9b576cf4558f4ac8c6
hamish <hamish.tennent@gmail.com>
2022-06-15 18:04:07 +0800

    init commit, copying over codepen code

---
+++ archive.js
@@ -0,0 +1,275 @@
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+const cardHolder = document.querySelector("div.card-holder");
+
+const addCard = document.querySelector("button.addCardButton");
+
+const itemArray = [...Array(10).keys()];
+
+const currentArrayItem = 0;
+
+const tags = document.querySelectorAll("div.tag");
+
+const modalTitle = document.querySelector("div.modal-title");
+const modalContent = document.querySelector("modal-content");
+const modalBackground = document.querySelector(".modal");
+
+let fadeTiming = 0;
+
+let taggedColours = [];
+
+let tagStatus = {
+  newzealand: false,
+  taiwan: false,
+  california: false,
+  earth: false,
+  tree: false,
+  grass: false
+};
+
+// addCard.addEventListener("click", () => {
+//   createCard();
+// });
+
+function createCard(item) {
+  let cardContainer = document.createElement("div");
+  let cardInner = document.createElement("div");
+  let cardContent = document.createElement("div");
+
+  cardContainer.className = "card";
+  cardInner.className = "card-inner";
+  cardContent.className = "card-content";
+
+  cardContent.textContent = item.place;
+  cardContainer.setAttribute("id", item.number);
+
+  cardContainer.appendChild(cardInner);
+  cardContainer.appendChild(cardContent);
+
+  cardInner.style.backgroundColor = item.background;
+
+  cardHolder.appendChild(cardContainer);
+}
+
+function hideCards() {
+  const cardsToShow = document.querySelectorAll("div.card");
+  cardsToShow.forEach((card) => {
+    card.classList.add("fadeOut");
+  });
+}
+
+function removeCards() {
+  const cardsToDelete = document.querySelectorAll("div.card");
+  cardsToDelete.forEach((card) => {
+    card.addEventListener("animationend", () => {
+      cardHolder.removeChild(card);
+    });
+  });
+}
+
+function showCards() {
+  const cardsToShow = document.querySelectorAll("div.card");
+  cardsToShow.forEach((card) => {
+    card.classList.add("fadeIn");
+  });
+}
+
+tags.forEach((tag) => {
+  tag.addEventListener("click", function () {
+    if (tag.classList.contains("active")) {
+      tag.classList.remove("active");
+      updateTagStatus(tag.innerHTML, false);
+      // tagStatus[0].nz = true
+      // console.log(tagStatus[0].nz)
+    } else {
+      tag.classList.add("active");
+      updateTagStatus(tag.innerHTML, true);
+      // tagStatus[0].nz = false
+      // console.log(tagStatus[0].nz)
+    }
+  });
+});
+
+function updateTagStatus(key, value) {
+  const grabbedHTMLTag = key;
+  const treatedHTMLTag = grabbedHTMLTag.replace(/\s/g, "");
+  const loweredHTMLTag = treatedHTMLTag.toLowerCase();
+  tagStatus[loweredHTMLTag] = value;
+  // console.log(key, "is set to", value);
+  filterColours();
+}
+
+function filterColours() {
+  hideCards();
+  removeCards();
+  let filteredColour = [];
+  let bufferColour = [];
+
+  if (tagStatus.newzealand == true) {
+    let addColour = colours.filter((col) => col.place === "New Zealand");
+    bufferColour = addColour;
+    addColour = checkMaterial(addColour);
+
+    addColour.forEach((item) => {
+      filteredColour.push(item);
+    });
+  }
+  if (tagStatus.taiwan == true) {
+    let addColour = colours.filter((col) => col.place === "Taiwan");
+    bufferColour = [...bufferColour, ...addColour];
+    addColour = checkMaterial(addColour);
+    addColour.forEach((item) => {
+      filteredColour.push(item);
+    });
+  }
+  if (tagStatus.california == true) {
+    let addColour = colours.filter((col) => col.place === "California");
+    bufferColour = [...bufferColour, ...addColour];
+    addColour = checkMaterial(addColour);
+    addColour.forEach((item) => {
+      filteredColour.push(item);
+    });
+  }
+
+  if (
+    filteredColour.length == 0 &&
+    tagStatus.tree == false &&
+    tagStatus.earth == false &&
+    tagStatus.grass == false
+  ) {
+    filteredColour = bufferColour;
+  }
+
+  if (
+    filteredColour.length == 0 &&
+    tagStatus.newzealand == false &&
+    tagStatus.taiwan == false &&
+    tagStatus.california == false
+  ) {
+    filteredColour = checkMaterial(colours);
+  }
+
+  if (filteredColour.length == 0) {
+    filteredColour = colours;
+  }
+
+  // Make sure there are no cards here, before making new ones, to prevent animation jumping
+
+  function animationCheck() {
+    const cardCheck = document.querySelectorAll("div.card");
+    if (cardCheck.length === 0) {
+      filteredColour.forEach((colour) => {
+        createCard(colour);
+      });
+      updateModals();
+      showCards();
+    } else {
+      setTimeout(animationCheck, 300);
+    }
+  }
+  
+  animationCheck()
+}
+
+function checkMaterial(addColour) {
+  let filteredMat = [];
+  if (tagStatus.earth == true) {
+    let addMat = addColour.filter((col) => col.material === "Earth");
+    // if (addMat.length == 0) {
+    //   return addMat;
+    // }
+    addMat.forEach((colour) => {
+      filteredMat.push(colour);
+    });
+  }
+
+  if (tagStatus.tree == true) {
+    let addMat = addColour.filter((col) => col.material === "Tree");
+    // if (addMat.length == 0) {
+    //   return addMat;
+    // }
+    addMat.forEach((colour) => {
+      filteredMat.push(colour);
+    });
+  }
+
+  if (tagStatus.grass == true) {
+    let addMat = addColour.filter((col) => col.material === "Grass");
+    // if (addMat.length == 0) {
+    //   return addMat;
+    // }
+    addMat.forEach((colour) => {
+      filteredMat.push(colour);
+    });
+  }
+
+  // if (filteredMat.length == 0) {
+  //   filteredMat = addColour;
+  // }
+
+  return filteredMat;
+}
+
+colours.forEach((colour) => {
+  createCard(colour);
+});
+
+showCards();
+
+updateModals();
+
+// Modal
+
+function updateModals() {
+  const openModalButtons = document.querySelectorAll("[data-modal-target]");
+  const closeModalButtons = document.querySelectorAll("[data-close-button]");
+  const overlay = document.getElementById("overlay");
+  const cardObjects = document.querySelectorAll("div.card");
+
+  cardObjects.forEach((button) => {
+    button.addEventListener("click", () => {
+      updateModalContent(button.getAttribute("id"));
+      openModal(modal);
+    });
+  });
+
+  openModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = document.querySelector(button.dataset.modalTarget);
+      openModal(modal);
+    });
+  });
+
+  overlay.addEventListener("click", () => {
+    const modals = document.querySelectorAll(".modal.active");
+    modals.forEach((modal) => {
+      closeModal(modal);
+    });
+  });
+
+  closeModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = button.closest(".modal");
+      closeModal(modal);
+    });
+  });
+
+  function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add("active");
+    overlay.classList.add("active");
+  }
+
+  function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+}
+
+function updateModalContent(number) {
+  modalTitle.textContent = colours[number].place;
+  modalBackground.style.backgroundColor = colours[number].background;
+}
